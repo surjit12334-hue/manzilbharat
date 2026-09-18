170b0e2d80c4a2d805aaae961f21499ac017984b
user <surjit123408@gmail.com>
2026-09-18 14:14:05 +0530

    fix: add port 3000 to CORS, restore DB password

--- app.py
+++ app.py
@@ -41,6 +41,8 @@ app = Flask(__name__, static_folder='.', static_url_path='')
 CORS(app, resources={r"/api/*": {"origins": [
     "http://localhost:5000",
     "http://localhost:8000",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:5000",
     "https://surjit12334-hue.github.io"
 ]}})
